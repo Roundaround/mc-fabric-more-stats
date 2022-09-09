6e04379841891c85e00c7f6eb1e32e50e3430189
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-09-09 09:15:12 -0400

    Only count quick transfers as renames when actual renames

--- src/main/java/me/roundaround/morestats/mixin/ForgingScreenHandlerMixin.java
+++ src/main/java/me/roundaround/morestats/mixin/ForgingScreenHandlerMixin.java
@@ -1,5 +1,7 @@
 package me.roundaround.morestats.mixin;
 
+import java.util.Optional;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,12 +15,34 @@ import net.minecraft.screen.ForgingScreenHandler;
 
 @Mixin(ForgingScreenHandler.class)
 public abstract class ForgingScreenHandlerMixin {
-  @Inject(method = "transferSlot", at = @At(value = "RETURN"))
-  public void onTakeItem(PlayerEntity player, int index, CallbackInfoReturnable<ItemStack> info) {
-    if (!(((Object) this) instanceof AnvilScreenHandler) || index != 2) {
+  Optional<ItemStack> input = Optional.empty();
+
+  @Inject(method = "transferSlot", at = @At(value = "HEAD"))
+  public void beforeTakeItem(PlayerEntity player, int index, CallbackInfoReturnable<ItemStack> info) {
+    ForgingScreenHandler self = (ForgingScreenHandler) (Object) this;
+    if (!(self instanceof AnvilScreenHandler) || index != 2) {
       return;
     }
 
-    player.increaseStat(MoreStats.ITEM_RENAME, info.getReturnValue().getCount());
+    input = Optional.of(self.getSlot(0).getStack().copy());
+  }
+
+  @Inject(method = "transferSlot", at = @At(value = "RETURN"))
+  public void onTakeItem(PlayerEntity player, int index, CallbackInfoReturnable<ItemStack> info) {
+    ForgingScreenHandler self = (ForgingScreenHandler) (Object) this;
+    if (!(self instanceof AnvilScreenHandler) || index != 2) {
+      return;
+    }
+
+    if (input.isEmpty()) {
+      return;
+    }
+
+    ItemStack stack = info.getReturnValue();
+    if (stack.hasCustomName() && !stack.getName().equals(input.get().getName())) {
+      player.increaseStat(MoreStats.ITEM_RENAME, stack.getCount());
+    }
+
+    input = Optional.empty();
   }
 }
